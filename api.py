9bd1d879b7dc99d102d63deda5a80cd2df6e7241
adammmmmmmmd <evg.omelchenko18@gmail.com>
2024-03-18 00:26:22 +0300

    deleted html + added currency calculation

--- api.py
+++ api.py
@@ -1,30 +1,32 @@
 import requests
-# from flask import Flask, render_template, request, redirect, abort, url_for
 
 
 def main():
-    base = input(f'Input currency to compare to:\n'
+    base = input(f'Input currency to convert from:\n'
                  f'1: EUR, 2: USD, 3: RUB\n')
     match base:
         case '1': base = "EUR"
         case '2': base = "USD"
         case '3': base = "RUB"
-    rate = input(f'Input currency to compare:\n'
+    rate = input(f'Input currency to convert into:\n'
                  f'1: EUR, 2: USD, 3: RUB\n')
     match rate:
         case '1': rate = "EUR"
         case '2': rate = "USD"
         case '3': rate = "RUB"
-    url = "https://api.apilayer.com/fixer/latest?base={}&symbols={}&apikey=BAvGlFFYWfKr5HynX6HpLXK0qt4us5Hw".format(base, rate)
+    url = f"https://api.apilayer.com/fixer/latest?base={base}&symbols={rate}&apikey=BAvGlFFYWfKr5HynX6HpLXK0qt4us5Hw"
     res = requests.get(url)
     if res.status_code != 200:
         raise Exception("ERROR: API request unsuccesful!")
     data = res.json()
-    print(url)
-    # currency_base = data["base"]
     currency_rate = data["rates"][rate]
-    print(res.status_code)
+    amount = input(
+        f'Input how much {base} '
+        f'would you like to convert into {rate}: '
+    )
+    converted_currency = round((float(currency_rate) * float(amount)), 2)
     print(f'1 {base} equals {currency_rate} {rate}')
+    print(f'{amount} {base} equals {converted_currency} {rate}')
 
 
 if __name__ == "__main__":
