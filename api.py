148910ff5e243fce5fc8b811c22737e5599640e1
adammmmmmmmd <evg.omelchenko18@gmail.com>
2024-03-17 23:53:36 +0300

    added url insertion + currency choices

--- api.py
+++ api.py
@@ -1,14 +1,30 @@
 import requests
+# from flask import Flask, render_template, request, redirect, abort, url_for
 
 
 def main():
-    res = requests.get("https://api.apilayer.com/fixer/latest?base=USD&symbols={}&apikey=BAvGlFFYWfKr5HynX6HpLXK0qt4us5Hw")
+    base = input(f'Input currency to compare to:\n'
+                 f'1: EUR, 2: USD, 3: RUB\n')
+    match base:
+        case '1': base = "EUR"
+        case '2': base = "USD"
+        case '3': base = "RUB"
+    rate = input(f'Input currency to compare:\n'
+                 f'1: EUR, 2: USD, 3: RUB\n')
+    match rate:
+        case '1': rate = "EUR"
+        case '2': rate = "USD"
+        case '3': rate = "RUB"
+    url = "https://api.apilayer.com/fixer/latest?base={}&symbols={}&apikey=BAvGlFFYWfKr5HynX6HpLXK0qt4us5Hw".format(base, rate)
+    res = requests.get(url)
     if res.status_code != 200:
-        raise Exception("ERROR: API request unsuccesful!")  
+        raise Exception("ERROR: API request unsuccesful!")
     data = res.json()
-    currency_base = data["base"]
-    currency_rate = data["rates"]["RUB"]
-    print(data, '\n', res.status_code)
+    print(url)
+    # currency_base = data["base"]
+    currency_rate = data["rates"][rate]
+    print(res.status_code)
+    print(f'1 {base} equals {currency_rate} {rate}')
 
 
 if __name__ == "__main__":
